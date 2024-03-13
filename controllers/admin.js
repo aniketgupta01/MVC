594f4cb5c3b13f602a56138db81cf0616b0a9dee
aniketgupta01 <aniketg2831@gmail.com>
2024-03-13 20:45:14 +0530

    till associations

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,7 +8,8 @@ exports.getAddProduct = (req, res, next) => {
     productCSS: true,
     activeAddProduct: true,
     editing:false
-  });
+  }).then(result => console.log(result))
+  .catch(err => console.log(err));
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -16,13 +17,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save()
-  .then(() =>{
-    res.redirect('/');
+ Product.create({
+  title:title,
+  price:price,
+  imageUrl:imageUrl,
+  description:description
 
-  })
-  .catch(err => console.log(err))
+ }).then(() => {
+  res.redirect('/');
+ })
+ .catch(err => console.log(err));
   
 };
 
@@ -32,7 +36,8 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product =>{
+  Product.findByPk(prodId)
+  .then( product =>{
     if(!product){
       return res.redirect('/');
     }
@@ -45,6 +50,7 @@ exports.getEditProduct = (req, res, next) => {
     });
 
   })
+  .catch(err => console.log(err))
   
 };
 
@@ -54,32 +60,41 @@ exports.postEditProduct = (req,res,next)=>{
   const updatedPrice = req.body.price;
   const UpdatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description; 
-  const updatedproduct = new Product(
-    prodId, 
-    updatedTitle, 
-    UpdatedImageUrl,
-    updatedDescription,
-    updatedPrice )
-
-    updatedproduct.save();
-    res.redirect('/admin/products')
+  Product.findByPk(prodId)
+  .then((product) => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = UpdatedImageUrl;
+    product.description = updatedDescription;
+    return product.save();
+    
+  })
+  .then(result => {console.log("EDITED")
+    res.redirect('/admin/products')})
+  .catch(err => console.log(err))
+  
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows]) => {
+  Product.findAll()
+  .then((products) => {
     res.render('admin/products', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
   })
+  .catch(err => console.log(err))
 };
 
 exports.postDeleteProduct = (req,res,next)=>{
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-  .then(() => {
+  Product.findByPk(prodId)
+  .then(product =>{
+    return product.destroy();
+  })
+  .then(result => {
+    console.log("DELETED")
     res.redirect('/admin/products');
   })
   .catch(err => console.log(err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -3,10 +3,10 @@ const Cart = require('../models/cart.js')
 const cart = require('../models/cart.js')
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) =>{
+  Product.findAll()
+  .then((products) => {
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
@@ -16,11 +16,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next)=>{
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product]) => {
+  Product.findByPk(prodId)
+  .then(product => {
     res.render('shop/product-detail',
-    {product: product[0], 
-    pageTitle:product[0].title,
+    {product: product, 
+    pageTitle:product.title,
     path: '/products'})
   })
   .catch(err => console.log(err));
@@ -28,10 +28,10 @@ exports.getProduct = (req, res, next)=>{
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData])=>{
+  Product.findAll()
+  .then((products) => {
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
